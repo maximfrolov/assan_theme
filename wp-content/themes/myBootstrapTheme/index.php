c51f0c1cf86924f0c84d1d8e76ffdcb87f78d31d
Maxim Frolov <consumer2@mail.ru>
2015-11-04 00:31:28 +0500

    removed the static connection of scripts and styles in index.php

--- wp-content/themes/myBootstrapTheme/index.php
+++ wp-content/themes/myBootstrapTheme/index.php
@@ -5,42 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>assan - Professional theme</title>
-
-    <!-- Bootstrap -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- custom css (blue color by default) -->
-    <link href="css/style.css" rel="stylesheet" type="text/css" media="screen">
-
-    <!-- font awesome for icons -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <!-- flex slider css -->
-    <link href="css/flexslider.css" rel="stylesheet" type="text/css" media="screen">
-    <!-- animated css  -->
-    <link href="css/animate.css" rel="stylesheet" type="text/css" media="screen">
-
-
-
-
-
-    <!--owl carousel css-->
-    <link href="css/owl.carousel.css" rel="stylesheet" type="text/css" media="screen">
-    <link href="css/owl.theme.css" rel="stylesheet" type="text/css" media="screen">
-    <!--mega menu -->
-    <link href="css/yamm.css" rel="stylesheet" type="text/css">
-    <!--popups css-->
-    <link href="css/magnific-popup.css" rel="stylesheet" type="text/css">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
     <?php wp_head(); ?>
 </head>
 <body>
-
-
 <div class="top-bar-light">
     <div class="container">
         <div class="row">
@@ -688,37 +655,6 @@
         </div>
     </div>
 </footer><!--default footer end here-->
-<!--scripts and plugins -->
-<!--must need plugin jquery-->
-<script src="js/jquery.min.js"></script>
-<!--bootstrap js plugin-->
-<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<!--easing plugin for smooth scroll-->
-<script src="js/jquery.easing.1.3.min.js" type="text/javascript"></script>
-<!--sticky header-->
-<script type="text/javascript" src="js/jquery.sticky.js"></script>
-<!--flex slider plugin-->
-<script src="js/jquery.flexslider-min.js" type="text/javascript"></script>
-<!--parallax background plugin-->
-<script src="js/jquery.stellar.min.js" type="text/javascript"></script>
-
-
-<!--digit countdown plugin-->
-<script src="js/waypoints.min.js"></script>
-<!--digit countdown plugin-->
-<script src="js/jquery.counterup.min.js" type="text/javascript"></script>
-<!--on scroll animation-->
-<script src="js/wow.min.js" type="text/javascript"></script>
-<!--owl carousel slider-->
-<script src="js/owl.carousel.min.js" type="text/javascript"></script>
-<!--popup js-->
-<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
-<!--you tube player-->
-<script src="js/jquery.mb.YTPlayer.min.js" type="text/javascript"></script>
-
-
-<!--customizable plugin edit according to your needs-->
-<script src="js/custom.js" type="text/javascript"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
