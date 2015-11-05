7755372f08624f678c8889f2c74aa64acd799e02
Maxim Frolov <consumer2@mail.ru>
2015-11-05 17:28:57 +0500

    inserted sidebar in sidebar.php

---
+++ wp-content/themes/myBootstrapTheme/sidebar.php
@@ -0,0 +1,73 @@
+<div class="col-md-3 col-md-offset-1">
+    <div class="sidebar-box margin40">
+        <h4>Search</h4>
+        <form role="form" class="search-widget">
+            <input type="text" class="form-control">
+            <i class="fa fa-search"></i>
+        </form>
+    </div><!--sidebar-box-->
+    <div class="sidebar-box margin40">
+        <h4>Text widget</h4>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lorem quam, adipiscing condimentum tristique vel, eleifend sed turpis. Pellentesque cursus arcu id magna euismod in elementum purus molestie.
+        </p>
+    </div><!--sidebar-box-->
+    <div class="sidebar-box margin40">
+        <h4>Categories</h4>
+        <ul class="list-unstyled cat-list">
+            <li> <a href="#">Marketing</a> <i class="fa fa-angle-right"></i></li>
+            <li> <a href="#">Videos</a> <i class="fa fa-angle-right"></i></li>
+            <li> <a href="#">Sports</a> <i class="fa fa-angle-right"></i></li>
+            <li> <a href="#">Fashion</a> <i class="fa fa-angle-right"></i></li>
+            <li> <a href="#">Media</a> <i class="fa fa-angle-right"></i></li>
+            <li> <a href="#">Seo</a> <i class="fa fa-angle-right"></i></li>
+            <li> <a href="#">Marketing</a> <i class="fa fa-angle-right"></i></li>
+        </ul>
+    </div><!--sidebar-box-->
+    <div class="sidebar-box margin40">
+        <h4>Popular Post</h4>
+        <ul class="list-unstyled popular-post">
+            <li>
+                <div class="popular-img">
+                    <a href="#"> <img src="<?php bloginfo('template_url'); ?>/img/img-7.jpg" class="img-responsive" alt=""></a>
+                </div>
+                <div class="popular-desc">
+                    <h5> <a href="#">blog post image</a></h5>
+                    <h6>31st july 2014</h6>
+                </div>
+            </li>
+            <li>
+                <div class="popular-img">
+                    <a href="#"> <img src="<?php bloginfo('template_url'); ?>/img/img-8.jpg" class="img-responsive" alt=""></a>
+                </div>
+                <div class="popular-desc">
+                    <h5> <a href="#">blog post image</a></h5>
+                    <h6>31st july 2014</h6>
+                </div>
+            </li>
+            <li>
+                <div class="popular-img">
+                    <a href="#"> <img src="<?php bloginfo('template_url'); ?>/img/img-9.jpg" class="img-responsive" alt=""></a>
+                </div>
+                <div class="popular-desc">
+                    <h5> <a href="#">blog post image</a></h5>
+                    <h6>31st july 2014</h6>
+                </div>
+            </li>
+        </ul>
+    </div><!--sidebar-box-->
+    <div class="sidebar-box margin40">
+        <h4>Tag Cloud</h4>
+        <div class="tag-list">
+            <a href="#">Wordpress</a>
+            <a href="#">Design</a>
+            <a href="#">Graphics</a>
+            <a href="#">Seo</a>
+            <a href="#">Development</a>
+            <a href="#">Marketing</a>
+            <a href="#">fashion</a>
+            <a href="#">Media</a>
+            <a href="#">Photoshop</a>
+        </div>
+    </div>
+</div><!--sidebar-col-->
