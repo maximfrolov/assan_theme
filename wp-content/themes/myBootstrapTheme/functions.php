7c22965e5158e0170253eca869a7f618b684b495
Maxim Frolov <consumer2@mail.ru>
2015-11-06 13:08:03 +0500

    added widgets in WordPress with the help of register_sidebar() in functions.php

--- wp-content/themes/myBootstrapTheme/functions.php
+++ wp-content/themes/myBootstrapTheme/functions.php
@@ -34,4 +34,21 @@ function loadStyleScript() {
  * подключаем скрипты и стили
  * */
 add_action('wp_enqueue_scripts', 'loadStyleScript');
+/*
+ * добавляем виджеты
+ * */
+register_sidebar([
+    'name' => 'Меню',
+    'id'   => 'menu_header',
+]);
 
+/*$args = [
+    'name'          => __( 'Sidebar name', 'theme_text_domain' ),
+    'id'            => 'unique-sidebar-id',
+    'description'   => '',
+    'class'         => '',
+    'before_widget' => '<li id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</li>',
+    'before_title'  => '<h2 class="widgettitle">',
+    'after_title'   => '</h2>'
+];*/
