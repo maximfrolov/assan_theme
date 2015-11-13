9f1d3d4d68aaf5f52dd5f2490c2ef387564b0414
Maxim Frolov <consumer2@mail.ru>
2015-11-13 11:43:11 +0500

    brought widgets sidebar footer

--- wp-content/themes/myBootstrapTheme/footer.php
+++ wp-content/themes/myBootstrapTheme/footer.php
@@ -1,26 +1,9 @@
 <footer id="footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-3 col-sm-6 margin30">
-                <?php if(!dynamic_sidebar('footer_1')) : ?>
-                    <h3>Здесь блок виджетов: Footer 1</h3>
-                <?php endif; ?>
-            </div>
-            <div class="col-md-3 col-sm-6 margin30">
-                <?php if(!dynamic_sidebar('footer_2')) : ?>
-                    <h3>Здесь блок виджетов: Footer 2</h3>
-                <?php endif; ?>
-            </div>
-            <div class="col-md-3 col-sm-6 margin30">
-                <?php if(!dynamic_sidebar('footer_3')) : ?>
-                    <h3>Здесь блок виджетов: Footer 3</h3>
-                <?php endif; ?>
-            </div>
-            <div class="col-md-3 col-sm-6 margin30">
-                <?php if(!dynamic_sidebar('footer_4')) : ?>
-                    <h3>Здесь блок виджетов: Footer 4</h3>
-                <?php endif; ?>
-            </div>
+            <?php if(!dynamic_sidebar('footer_1')) : ?>
+                <h3>Здесь блок виджетов: Footer</h3>
+            <?php endif; ?>
         </div>
         <div class="row">
             <div class="col-md-12 text-center">
