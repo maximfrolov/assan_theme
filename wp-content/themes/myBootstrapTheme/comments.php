84a67ac509685d100516f68c8ca8e2c0026fadc7
Maxim Frolov <consumer2@mail.ru>
2015-11-17 12:00:49 +0500

    created a file, added a markup and pagination

--- wp-content/themes/myBootstrapTheme/comments.php
+++ wp-content/themes/myBootstrapTheme/comments.php
@@ -1,7 +1,61 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Максим
- * Date: 17.11.2015
- * Time: 10:27
- */
+<div class="comment-post">
+    <h3>3 Comments</h3>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="comment-list">
+                <h4><img src="img/team-2.jpg" class="img-responsive" alt="">
+                    by User <a href="#" class="btn btn-xs btn-theme-dark">Reply</a>
+                </h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lorem quam, adipiscing condimentum tristique vel, eleifend sed turpis. Pellentesque cursus arcu id magna euismod in elementum purus molestie
+                </p>
+            </div><!--comments list-->
+        </div>
+        <div class="col-md-10 col-md-offset-1">
+            <div class="comment-list">
+                <h4><img src="img/team-3.jpg" class="img-responsive" alt="">
+                    by User <a href="#" class="btn btn-xs btn-theme-dark">Reply</a>
+                </h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lorem quam, adipiscing condimentum tristique vel, eleifend sed turpis. Pellentesque cursus arcu id magna euismod in elementum purus molestie
+                </p>
+            </div><!--comments list-->
+        </div>
+        <div class="col-md-12">
+            <div class="comment-list">
+                <h4><img src="img/team-4.jpg" class="img-responsive" alt="">
+                    by User <a href="#" class="btn btn-xs btn-theme-dark">Reply</a>
+                </h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lorem quam, adipiscing condimentum tristique vel, eleifend sed turpis. Pellentesque cursus arcu id magna euismod in elementum purus molestie
+                </p>
+            </div><!--comments list-->
+        </div>
+    </div>
+</div><!--comments-->
+<ul class="pager">
+    <li class="previous"><?php previous_post_link('%link', '&larr; Предыдущий пост') ?></li>
+    <li class="next"><?php next_post_link('%link', 'Следующий пост &rarr;') ?></li>
+</ul><!--pager-->
+<div class="divide60"></div>
+<div class="comment-form">
+    <h3>Leave Comment</h3>
+    <div class="form-contact">
+        <form role="form">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="email" class="form-control" id="name" required="">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="password" class="form-control" id="email" required="">
+            </div>
+            <div class="form-group">
+                <label for="message">Comment</label>
+                <textarea class="form-control" rows="7" id="message" required=""></textarea>
+            </div>
+            <button type="submit" class="btn btn-theme-bg btn-lg pull-right">Comment</button>
+        </form>
+    </div>
+</div>
+<div class="divide60"></div>
